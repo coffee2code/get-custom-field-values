67a2af8de1816a5cb3296565816d959febdbe3a0
Scott Reilly <scott@coffee2code.com>
2014-01-17 02:47:32 -0700

    Change referenced filename in tests/bootstrap.php due to file renaming
    Also regenerate .pot

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../get-custom.php';
+	require dirname( __FILE__ ) . '/../get-custom-field-values.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
